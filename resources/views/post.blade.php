cbf76d4b5d5557db50ec0632b99f27056d78a0ad
rafaellamgs <rafaellamgsouza@gmail.com>
2022-09-22 22:26:39 -0300

    Storing blog posts as html files class

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+
+    <article>
+
+        <?= $post; ?>
+        
+    </article>
+
+    <a href="/">Go back</a>
+
+</body>
+
+</html>
