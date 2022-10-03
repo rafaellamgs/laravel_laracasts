d15a48da7e42c5ae08d81583efc3dc62376ef175
rafaellamgs <rafaellamgsouza@gmail.com>
2022-10-03 17:01:28 -0300

    Use the Filesystem Class to Read a Directory class.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,23 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('posts');
+    
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-
-        return redirect('/');
-    }
-    
-    //-------- without using arrow function -----------//
-    // $post = cache()->remember("posts.{$slug}", 120, function () use ($path) {
-        
-        //     return file_get_contents($path);
-        // });
-        
-   $post = cache()->remember("posts.{$slug}", 120, fn() =>file_get_contents($path));
-
-    return view('post', ['post' => $post]);
+   return view('post', [
+    'post' => Post::find($slug)
+   ]);
+   
 })->where('post', '[A-z_\-]+');
