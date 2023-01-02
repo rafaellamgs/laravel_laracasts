57d775791da489b87f4f4c0d25458ba4cfbe1bee
rafaellamgs <rafaellamgsouza@gmail.com>
2023-01-02 20:27:15 -0300

    Integrate the design begining

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,21 @@
-@extends('layout')
+<x-layout>
+@include('_posts-header')
 
-@section('content')
-    
-  
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+       @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
 
-    @foreach ($posts as $post) 
-
-    <article>
-
-       <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {!! $post->title !!}
-            </a>
+            @if (posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post" />
+                    @endforeach                
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please, come back later.</p>
+        @endif
             
-       </h1>
-
-       <p>
-          By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name }}</a>
-       </p>
-        
-       <div>
-            {{ $post->excerpt }}
-       </div>
-
-    </article>
-
-    @endforeach
-
-@endsection 
+    </main>
+    
+</x-layout>
